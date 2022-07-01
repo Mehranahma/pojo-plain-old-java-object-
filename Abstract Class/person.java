c4d4a92326751b35eeec685d7c93b1953ecb5a3e
Mehran ahmad <95295748+Mehranahma@users.noreply.github.com>
2022-07-01 22:35:34 +0530

    Add files via upload

---
+++ Abstract Class/person.java
@@ -0,0 +1,16 @@
+public abstract  class person {
+ public abstract String getDescription();
+ private String name;
+
+ public person(String name)
+ {
+
+     this.name=name;
+ }
+
+ public String getName()
+ {
+     return name;
+ }
+
+}
